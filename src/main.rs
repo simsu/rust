c50ca16df4b13148346186073b3fedbe3e0b9d3f
HyeonCheol Nam <namhc302@gmail.com>
2022-05-09 16:34:59 +0900

    guide

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use actix_web::{web, App, HttpServer};
+use deadpool_postgres::{Config, ManagerConfig, Pool, Runtime};
+use tokio_postgres::NoTls;
+
+mod http;
+
+fn env(key: &str) -> Option<String> {
+    match std::env::var(key) {
+        Ok(text) => Some(text),
+        _ => None,
+    }
+}
+
+fn establish_pool() -> Pool {
+    let mut cfg = Config::new();
+    cfg.dbname = env("DB_NAME");
+    cfg.user = env("DB_USERNAME");
+    cfg.password = env("DB_PASSWORD");
+    cfg.host = env("DB_HOST");
+    cfg.manager = Some(ManagerConfig {
+        recycling_method: deadpool_postgres::RecyclingMethod::Fast,
+    });
+    cfg.create_pool(Some(Runtime::Tokio1), NoTls)
+        .expect("DB Connection Failed")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .app_data(web::Data::new(establish_pool()))
+            .configure(http::config)
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
+}
